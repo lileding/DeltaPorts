54a6ef200baff76eda13710047f7a102e9b31fb5
LI Leding <lileding@gmail.com>
2026-07-27 20:12:42 +0800

    ports/multimedia/libv4l: consolidate DeltaPorts changes (libv4l-1.23.0_5)

--- ports/multimedia/libv4l/dragonfly/patch-utils_dvb_dvbv5-daemon.c
+++ ports/multimedia/libv4l/dragonfly/patch-utils_dvb_dvbv5-daemon.c
@@ -1,11 +1,12 @@
---- utils/dvb/dvbv5-daemon.c.intermediate	2020-08-16 06:52:14.000000000 +0000
+--- utils/dvb/dvbv5-daemon.c.orig
 +++ utils/dvb/dvbv5-daemon.c
-@@ -18,7 +18,7 @@
-  *
-  */
- 
--#ifndef __FreeBSD__
-+#if !defined(__FreeBSD__) && !defined(__DragonFly__)
- #define _XOPEN_SOURCE 600
+@@ -23,6 +23,9 @@
+ #  if __FreeBSD_version < 1500505 || (__FreeBSD_version >= 1600000 && __FreeBSD_version < 1600008)
+ #    define tdestroy(...) do {} while (0)
+ #  endif
++#elif defined(__DragonFly__)
++/* DragonFly libc has no tdestroy() */
++#  define tdestroy(...) do {} while (0)
  #else
- #define	tdestroy(...) do {} while (0)
+ #  define _XOPEN_SOURCE 600
+ #endif
